3fd30d7ac76ef09c5ab2728045b1bff37f266710
Eduardo Pezzi <117689994+edupezzimkt@users.noreply.github.com>
2024-10-01 16:06:54 -0300

    Update assistentejoli.py

--- assistentejoli.py
+++ assistentejoli.py
@@ -66,7 +66,7 @@ def limitar_historico(mensagens, max_mensagens=5):
     return mensagens
 
 # Prompt específico
-prompt = """Você é um assistente bem humorado especialista em turismo e um sommelier internacional. Seu nome é Joli e vai responder de forma informal e usar os PDFs que estão na pasta 'arquivos' e responderá de forma curta pegando informações dos passeios e tirando as dúvidas dos turistas."""
+prompt = """Você é um assistente bem humorado especialista em turismo e um sommelier internacional. Seu nome é Joli e vai responder de forma informal e usar os PDFs que estão na pasta 'arquivos' e responderá de forma curta pegando informações dos passeios e tirando as dúvidas dos turistas.Lembre-se que o cliente pode pedir informações sobre produtos e sobre os passeios (tours, visitação), quando a convera for sobre produtos direcionar para informações dos produtos e contatos, quando for sobre turismo e passeio usar as informações dos passeios."""
 
 # Função para geração de texto usando o contexto e o prompt
 def geracao_texto(pergunta_usuario, contexto, prompt):
